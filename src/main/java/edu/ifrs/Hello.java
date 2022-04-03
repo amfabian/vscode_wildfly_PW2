e2b5d7d65046f00af1b5da551cfdf13b7bd56066
Alexandre Fabian <xandy.fabian@gmail.com>
2022-04-03 17:05:38 -0300

    Criado hello com javax ws rs api

--- src/main/java/edu/ifrs/Hello.java
+++ src/main/java/edu/ifrs/Hello.java
@@ -1,8 +1,14 @@
 package edu.ifrs;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
 
-
-
+@Path("/hello")
 public class Hello {
+
+    @GET
+    public String hello(){
+        return "hello ws";
+    }
     
 }
