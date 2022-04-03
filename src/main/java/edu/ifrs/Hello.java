217a3cf204fb40a26f9448ac602f1794e73da38e
Alexandre Fabian <xandy.fabian@gmail.com>
2022-04-03 17:35:27 -0300

    Configurado Stateless

--- src/main/java/edu/ifrs/Hello.java
+++ src/main/java/edu/ifrs/Hello.java
@@ -1,14 +1,16 @@
 package edu.ifrs;
 
+import javax.ejb.Stateless;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 
 @Path("/hello")
+@Stateless
 public class Hello {
 
     @GET
     public String hello(){
-        return "hello ws";
+        return "hello ws Stateless";
     }
     
 }
