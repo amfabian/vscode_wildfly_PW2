9336a8a79767b691537093a4d5eb9d2b09ff259a
Alexandre Fabian <xandy.fabian@gmail.com>
2022-04-03 17:12:17 -0300

    add Configuration.java

---
+++ src/main/java/edu/ifrs/Configuration.java
@@ -0,0 +1,22 @@
+package edu.ifrs;
+
+import java.util.Set;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath(value = "/ws")
+public class Configuration  extends Application{
+    
+
+    @Override
+    public Set<Class<?>> getClasses(){
+        Set<Class<?>> resources = new java.util.HashSet<Class<?>>();
+        addRestResourceClasses(resources);
+        return resources;
+    }
+}
+
+private void addRestResourceClasses(Set<Class<?>> resources) {
+    resources.add(edu.ifrs.Hello.class);
+}
